3e29f6fa4644aff9b43f30277a94b49b6a335ab2
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2022-07-28 19:39:00 +0900

    CT_1D

---
+++ PGM/Java/src/lv1/Start.java
@@ -0,0 +1,27 @@
+package lv1;
+
+public class Start {
+    public static void main(String[] args) {
+
+//        new 직사각형_별찍기();
+
+//        x만큼_간격이_있는_n개의_숫자 xn = new x만큼_간격이_있는_n개의_숫자();
+//        for (long l : xn.solution(2, 5)) {
+//            System.out.print(l + " ");
+//        }
+
+//        행렬의_덧셈 arr1arr2 = new 행렬의_덧셈();
+//        int[][] arr1 = {{1,2},{2,3}};
+//        int[][] arr2 = {{3,4},{5,6}};
+//        for (int[] i : arr1arr2.solution(arr1, arr2)) {
+//            for(int j : i){
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//        }
+
+        핸드폰_번호_가리기 s = new 핸드폰_번호_가리기();
+        System.out.println(s.solution("027778888"));
+
+    }
+}
