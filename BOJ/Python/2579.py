923cfd2c7513a9ff1f918357f42fd4d2f0dd4914
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2022-09-01 23:53:00 +0900

    CT_19

--- BOJ/Python/2579.py
+++ BOJ/Python/2579.py
@@ -1,13 +1,24 @@
-for _ in range(int(input())):
-    n = int(input())
-    weardict = {}
-    for _ in range(n):
-        wear = list(input().split())
-        if wear[1] in weardict:
-            weardict[wear[1]].append(wear[0])
-        else:
-            weardict[wear[1]] = [wear[0]]
-    cnt = 1 
-    for k in weardict:
-        cnt *= (len(weardict[k])+1)
-    print(cnt-1)
+n = int(input())
+
+arr = []
+dp = [0] * n
+
+for _ in range(n):
+    arr.append(int(input()))
+
+dp[0] = arr[0]
+if n == 1:
+    print(sum(arr))
+    exit(0)
+dp[1] = max(arr[0]+arr[1], arr[1])
+if n == 2:
+    print(sum(arr))
+    exit(0)
+dp[2] = max(arr[0]+arr[2], arr[1]+arr[2])
+
+for i in range(3, n):
+    dp[i] = max(dp[i-2]+arr[i], dp[i-3]+arr[i-1]+arr[i])
+
+print(dp[n-1])
+print(arr)
+print(dp)
