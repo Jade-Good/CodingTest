b00e01426e790aa9509449eb705569ee1ce29eb7
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2022-10-17 21:28:48 +0900

    CT_64

--- BOJ/Java/src/S5/boj_1402.java
+++ BOJ/Java/src/S5/boj_1402.java
@@ -3,35 +3,16 @@ package S5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class boj_1402 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int arr[] = new int[n];
-        int mv = 0 ,mv_idx = 0;
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            if (arr[i] >= mv) {
-                mv = arr[i];
-                mv_idx = i;
-            }
-        }
+        int t = Integer.parseInt(br.readLine());
 
-        int answer = 0;
-        while (mv_idx > 0){
-            arr[0]++;
-            arr[mv_idx]--;
-            answer++;
-            mv = 0;
-            mv_idx = 0;
-            for (int i = 0; i < n; i++){
-                if (arr[i] >= mv){
-                    mv = arr[i];
-                    mv_idx = i;
-                }
-            }
+        for (int i = 0; i < t; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            System.out.println("yes");
         }
-        System.out.println(answer);
     }
 }
