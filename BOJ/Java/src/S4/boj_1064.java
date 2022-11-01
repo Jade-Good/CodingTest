6f45f62e78bbb881bd08079370021bc9872a5c0d
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2022-11-01 16:03:53 +0900

    CT_76

--- BOJ/Java/src/S4/boj_1064.java
+++ BOJ/Java/src/S4/boj_1064.java
@@ -13,13 +13,17 @@ public class boj_1064 {
         for (int i = 0; i < 6; i++)
             arr[i] = Integer.parseInt(st.nextToken());
 
-        double abx = (arr[0]-arr[2]);
-        double aby = (arr[1]-arr[3]);
-        double acx = (arr[0]-arr[4]);
-        double acy = (arr[1]-arr[5]);
-        double bcx = (arr[2]-arr[4]);
-        double bcy = (arr[3]-arr[5]);
+        double abx = arr[0]-arr[2];
+        double aby = arr[1]-arr[3];
+        double acx = arr[0]-arr[4];
+        double acy = arr[1]-arr[5];
+        double bcx = arr[2]-arr[4];
+        double bcy = arr[3]-arr[5];
 
+        // 기울기가 같으면 같은 직선에 존재하는 점
+        // 기울기 : x증가량/y증가량
+        // abx / aby == acx / acy (0을 나누면 오류가 생기므로 치환)
+        // abx * acy == acx * aby
         if(abx*acy == aby*acx){
             System.out.println(-1.0);
             System.exit(0);
@@ -29,13 +33,13 @@ public class boj_1064 {
         double ac_length = Math.sqrt(Math.pow(acx,2)+Math.pow(acy,2));
         double bc_length = Math.sqrt(Math.pow(bcx,2)+Math.pow(bcy,2));
 
-        double max = ab_length;
-        double min = ab_length;
+        double max = ab_length + ac_length;
+        double min = ab_length + ac_length;
 
-        if (ac_length > max) max = ac_length;
-        if (bc_length > max) max = ac_length;
-        if (ac_length < min) min = ac_length;
-        if (bc_length < min) min = ac_length;
+        if (ac_length + bc_length > max) max = ac_length + bc_length;
+        if (bc_length + ab_length > max) max = bc_length + ab_length;
+        if (ac_length + bc_length < min) min = ac_length + bc_length;
+        if (bc_length + ab_length < min) min = bc_length + ab_length;
 
         System.out.println((max-min)*2);
     }
