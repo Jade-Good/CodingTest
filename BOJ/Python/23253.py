ffd7099bf5c2a357e6061001cc25d44881fa18c2
BreathIN423 <kangsm423@gmail.com>
2022-11-18 01:10:54 +0900

    CT_89

--- BOJ/Python/23253.py
+++ BOJ/Python/23253.py
@@ -1,20 +1,12 @@
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
-
-int(input())
-stack1 = list(map(int,input().split()))
-int(input())
-stack2 = list(map(int,input().split()))
-
-print(stack1)
-print(stack1)
-
-for i in range(1,n+1):
-    if stack1[-1] == i:
-        stack1.pop()
-    elif stack2[-1] == i:
-        stack2.pop()
-    else:
-        print("No")
-        break
-else:
-    print("Yes")
+for _ in range(m):
+    book = int(input())
+    books = list(map(int,input().split()))
+    for i in range(book-1):
+        if books[i] < books[i+1]:
+            print("No")
+            exit(0)
+print("Yes")
