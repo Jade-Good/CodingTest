6666afedd3c71008d926b172ba84c0727906f677
Soomin_Kang <36834513+BreathIN423@users.noreply.github.com>
2023-01-05 17:55:01 +0900

    CT_124

---
+++ BOJ/Java/src/S3/boj_1166.java
@@ -0,0 +1,16 @@
+package S3;
+
+import java.util.Scanner;
+
+public class boj_1166 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int l = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+
+
+
+    }
+}
