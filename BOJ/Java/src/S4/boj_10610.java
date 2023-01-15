24db164c989407fdaef010761c36b4ef3f77917b
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2023-01-15 23:42:18 +0900

    CT_132

--- BOJ/Java/src/S4/boj_10610.java
+++ BOJ/Java/src/S4/boj_10610.java
@@ -1,20 +1,30 @@
 package S4;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class boj_10610 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        String[] arr = new String[str.length()];
-        for (int i = 0; i < str.length(); i++) {
-            arr[i] = str.substring(i);
-        }
-        Arrays.sort(arr);
+        String s = sc.next();
+        char[] charArr = s.toCharArray();
 
-        for (String s : arr) {
-            System.out.println(s);
+        Arrays.sort(charArr);
+        s = new StringBuilder(new String(charArr)).reverse().toString();
+
+        if (s.charAt(s.length() - 1) == '0') {
+            long sum = 0;
+            for (int i = 0; i < s.length(); i++) {
+                sum += s.charAt(i) - '0';
+            }
+            if (sum % 3 == 0) {
+                System.out.println(s);
+            } else {
+                System.out.println(-1);
+            }
+        } else {
+            System.out.println(-1);
         }
     }
 }
