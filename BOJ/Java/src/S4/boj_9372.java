33132e68de3faeb5d38103fb33ba5052b5f55940
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2023-01-24 23:40:42 +0900

    CT_141

---
+++ BOJ/Java/src/S4/boj_9372.java
@@ -0,0 +1,19 @@
+package S4;
+
+import java.util.Scanner;
+
+public class boj_9372 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t = 0; t < T; t++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            for (int i = 0; i < m; i++) {
+                sc.nextInt();
+                sc.nextInt();
+            }
+            System.out.println(n-1);
+        }
+    }
+}
