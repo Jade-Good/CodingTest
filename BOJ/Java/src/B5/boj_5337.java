620a229e12d2ceffe95f991ce65975d6b189ba6a
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2023-02-01 23:45:22 +0900

    CT_148

---
+++ BOJ/Java/src/B5/boj_5337.java
@@ -0,0 +1,9 @@
+package B5;
+
+public class boj_5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n"
+                + "|  | _ | _. _ ._ _  _\n"
+                + "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
