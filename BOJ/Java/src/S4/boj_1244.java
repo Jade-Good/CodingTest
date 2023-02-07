fd7376f33fb827cb0fc8d0be455fa53845b0bdc1
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2023-02-07 23:13:13 +0900

    CT_0207 + out 제거

--- BOJ/Java/src/S4/boj_1244.java
+++ BOJ/Java/src/S4/boj_1244.java
@@ -7,36 +7,32 @@ public class boj_1244 {
         // Input
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        boolean[] sw = new boolean[n];
+        boolean[] sw = new boolean[n+1];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             if (sc.nextInt() == 1)
                 sw[i] = true;
         }
 
         int m = sc.nextInt();
         for (int i = 0; i < m; i++) {
-            print(sw);
+//            print(sw);
             int gender = sc.nextInt();
             int idx = sc.nextInt();
 
             if (gender == 1) {
                 for (int j = 1; j <= n; j++) {
                     if (j % idx == 0)
-                        sw[j-1] = !sw[j-1];
+                        sw[j] = !sw[j];
                 }
             }
             else {
                 int left = idx;
                 int right = idx;
 
-                while(left >= 0 && right < n && sw[left] == sw[right]) {
+                while(left-1 > 0 && right+1 <= n && sw[left-1] == sw[right+1]) {
                     left--;
                     right++;
-                }
-                if (left < idx) {
-                    left++;
-                    right--;
                 }
                 for (int j = left; j <= right; j++) {
                     sw[j] = !sw[j];
@@ -46,12 +42,16 @@ public class boj_1244 {
         print(sw);
     }
     private static void print(boolean[] sw) {
-        System.out.println();
-        for (int i = 0; i < sw.length; i++) {
+        for (int i = 1; i < sw.length; i++) {
             if (sw[i])
-                System.out.print(1 + " ");
+                System.out.print(1);
             else
-                System.out.print(0 + " ");
+                System.out.print(0);
+            if (i % 20 == 0) {
+                System.out.println();
+            } else if (i != sw.length - 1){
+                System.out.print(" ");
+            }
         }
     }
 }
