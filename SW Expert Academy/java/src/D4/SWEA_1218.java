85242336f1ec3e0e12e992eac89301e01ed0db10
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2023-02-10 23:47:43 +0900

    CT_157

--- SW Expert Academy/java/src/D4/SWEA_1218.java
+++ SW Expert Academy/java/src/D4/SWEA_1218.java
@@ -7,24 +7,46 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class SWEA_1218 {
+    static int n;
+    static String str;
+    static Stack<Character> stack;
+
     public static void test() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int answer = 0;
 
         for (int t = 1; t <= 10; t++) {
-            int n = Integer.parseInt(br.readLine());
-            String str = br.readLine();
-            Stack<Character> stack = new Stack<>();
-
-            for (int i = 0; i < n; i++) {
-                char c = str.charAt(i);
-                if (c == ']' || c == '}' || c == '>' || c == ')'){
-                    char pop_char = stack.pop();
-                }
-            }
-
-
-            System.out.println("#" + t + " " + answer);
+            n = Integer.parseInt(br.readLine());
+            str = br.readLine();
+            stack = new Stack<>();
+            System.out.println("#" + t + " " + check_stack());
         }
+    }
+
+    private static int check_stack() {
+        char c;
+        for (int i = 0; i < n; i++) {
+            c = str.charAt(i);
+            switch (c) {
+                case ']':
+                    if (stack.pop() != '[')
+                        return 0;
+                    break;
+                case '}':
+                    if (stack.pop() != '{')
+                        return 0;
+                    break;
+                case '>':
+                    if (stack.pop() != '<')
+                        return 0;
+                    break;
+                case ')':
+                    if (stack.pop() != '(')
+                        return 0;
+                    break;
+                default:
+                    stack.add(c);
+            }
+        }
+        return 1;
     }
 }
