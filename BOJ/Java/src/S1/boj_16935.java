0ddc1f2763a8225121977b00a54be08a09ee067f
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2023-02-16 00:59:41 +0900

    CT_162

--- BOJ/Java/src/S1/boj_16935.java
+++ BOJ/Java/src/S1/boj_16935.java
@@ -6,10 +6,10 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class boj_16935 {
-    static int n, m, before_x, before_y, after_x, after_y, delta;
+    static int n, m;
     static int[][] arr, temp;
-    static int[] dx = {-1, 0, 1, 0}; // 상 우 하 좌
-    static int[] dy = {0, 1, 0, -1};
+//    static int[] dx = {-1, 0, 1, 0}; // 상 우 하 좌
+//    static int[] dy = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
         // input
@@ -27,9 +27,10 @@ public class boj_16935 {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < r; i++) {
 //            print();
-            int order = Integer.parseInt(br.readLine());
+            int order = Integer.parseInt(st.nextToken());
             switch (order) {
                 case 1:
                     updown();
@@ -39,9 +40,11 @@ public class boj_16935 {
                     break;
                 case 3:
                     turn_right();
+                    swap();
                     break;
                 case 4:
                     turn_left();
+                    swap();
                     break;
                 case 5:
                     rotate_right();
@@ -50,8 +53,14 @@ public class boj_16935 {
                     rotate_left();
                     break;
             }
-            print();
         }
+        print();
+    }
+
+    private static void swap() {
+        int nm = n;
+        n = m;
+        m = nm;
     }
 
     private static void print() {
@@ -62,6 +71,7 @@ public class boj_16935 {
             }
             sb.append('\n');
         }
+        sb.deleteCharAt(sb.length() - 1);
         System.out.println(sb);
     }
 
@@ -86,70 +96,105 @@ public class boj_16935 {
     }
 
     private static void turn_right() { // (3) 오른쪽 90도 회전
-        temp = new int[n][m];
+        temp = new int[m][n];
         int deapth = Math.min(n, m) / 2;
 
         for (int i = 0; i < deapth; i++) {
-            before_x = i;
-            before_y = i;
-            after_x = i;
-            after_y = m - i - 1;
+            int before_x = i;
+            int before_y = i;
+            int after_x = i;
+            int after_y = n - i - 1;
 
-            int count = 0;
-            delta = 3; // 좌
-
-            System.out.println(before_x + " " + before_y + " " + after_x + " " + after_y);
-
-            for (int j = 0; j < n - i * 2; j++) { // 왼쪽 면
-                temp[after_x][after_y] = arr[before_x++][before_y];
-                count = move(count, -1);
+            for (int j = 1; j < n - i * 2; j++) { // 왼쪽 면 -> 위쪽 면
+                temp[after_x][after_y--] = arr[before_x++][before_y];
             }
-            before_x--;
-            before_d + 1;
 
-            for (int j = 0; j < m - i * 2; j++) { // 아래 면 -> 왼쪽 면
+            for (int j = 1; j < m - i * 2; j++) { // 아래 면 -> 왼쪽 면
                 temp[after_x++][after_y] = arr[before_x][before_y++];
             }
-            after_x++;
-            before_y--;
 
-            for (int j = 0; j < n - i * 2; j++) { // 오른쪽 면 -> 아래 면
+            for (int j = 1; j < n - i * 2; j++) { // 오른쪽 면 -> 아래 면
                 temp[after_x][after_y++] = arr[before_x--][before_y];
             }
-            after_y--;
-            before_x++;
 
-            for (int j = 0; j < n - i * 2; j++) { // 위쪽 면 -> 오른쪽 면
+            for (int j = 1; j < m - i * 2; j++) { // 위쪽 면 -> 오른쪽 면
                 temp[after_x--][after_y] = arr[before_x][before_y--];
             }
         }
-
+        arr = temp;
     }
 
-    private static void turn_left() {
+    private static void turn_left() { // (4) 왼쪽 90도 회전
+        temp = new int[m][n];
+        int deapth = Math.min(n, m) / 2;
 
+        for (int i = 0; i < deapth; i++) {
+            int before_x = i;
+            int before_y = i;
+            int after_x = m - i - 1;
+            int after_y = i;
+
+            for (int j = 1; j < m - i * 2; j++) { // 왼쪽 면 <- 위쪽 면
+                temp[after_x--][after_y] = arr[before_x][before_y++];
+            }
+
+            for (int j = 1; j < n - i * 2; j++) { // 위쪽 면 <- 오른쪽 면
+                temp[after_x][after_y++] = arr[before_x++][before_y];
+            }
+
+            for (int j = 1; j < m - i * 2; j++) { // 오른쪽 면 <- 아래 면
+                temp[after_x++][after_y] = arr[before_x][before_y--];
+            }
+
+            for (int j = 1; j < n - i * 2; j++) { // 아래 면 <- 왼쪽 면
+                temp[after_x][after_y--] = arr[before_x--][before_y];
+            }
+        }
+        arr = temp;
     }
 
-    private static int move(int count, int flag) {
-        int nm = (delta == 0 || delta == 2) ? n : m;
-
-        if (count + flag == nm) {
-            delta = (delta + flag) % 4;
-            if (delta < 0) delta = 4+delta;
-            return 0;
-        } else {
-            after_x += dx[delta];
-            after_y += dy[delta];
-            return count + 1;
+    // 가로 세로 크기가 똑같고, 값만 돌려야 하는 줄....
+//    private static void move(int flag, int i) {
+//        int nm = (delta == 0 || delta == 2) ? n : m;
+//
+//        if (count + 1 == nm - i * 2) {
+//            delta = (delta + flag) % 4;
+//            if (delta < 0) {
+//                delta = 4 + delta;
+//            }
+//            count = 0;
+//        }
+//        after_x += dx[delta];
+//        after_y += dy[delta];
+//        count++;
+//    }
+    private static void rotate(int before_x, int before_y, int after_x, int after_y) {
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < m / 2; j++) {
+                temp[after_x + i][after_y + j] = arr[before_x + i][before_y + j];
+            }
         }
     }
 
     private static void rotate_right() {
+        temp = new int[n][m];
 
+        rotate(0, 0, 0, m / 2);       // 좌상 -> 우상
+        rotate(n / 2, 0, 0, 0);       // 좌하 -> 좌상
+        rotate(n / 2, m / 2, n / 2, 0);   // 우하 -> 좌하
+        rotate(0, m / 2, n / 2, m / 2);   // 우상 -> 우하
+
+        arr = temp;
     }
 
     private static void rotate_left() {
+        temp = new int[n][m];
 
+        rotate(0, m / 2, 0, 0);       // 좌상 <- 우상
+        rotate(0, 0, n / 2, 0);       // 좌하 <- 좌상
+        rotate(n / 2, 0, n / 2, m / 2);   // 우하 <- 좌하
+        rotate(n / 2, m / 2, 0, m / 2);   // 우상 <- 우하
 
+        arr = temp;
     }
 }
