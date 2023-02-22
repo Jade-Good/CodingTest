d45f0da0c273675df43954d6437c10b0743baed2
BreathIN423 <kangsm423@gmail.com>
2023-02-22 23:41:41 +0900

    CT_169

--- BOJ/Java/src/G4/Boj_9663_N_Queen.java
+++ BOJ/Java/src/G4/Boj_9663_N_Queen.java
@@ -1,7 +1,40 @@
 package G4;
 
-public class Boj_9663_N_Queen {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Boj_9663_N_Queen {
+    private static int N, answer;
+    private static int[] result;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        result = new int[N];
+
+        backtraking(0);
+
+        System.out.println(answer);
+    }
+
+    private static void backtraking(int idx) {
+        if (idx == N) {
+            answer++;
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (check(idx, i)) {
+                result[idx] = i;
+                backtraking(idx + 1);
+            }
+        }
+    }
+
+    private static boolean check(int idx, int i) {
+        for (int j = 0; j < idx; j++) {
+            if (result[j] == i || Math.abs(j - idx) == Math.abs(result[j] - i))
+                return false;
+        }
+        return true;
     }
 }
--- SW Expert Academy/java/src/D3/SWEA_1873_상호의_배틀필드.java
+++ SW Expert Academy/java/src/D3/SWEA_1873_상호의_배틀필드.java
@@ -6,16 +6,17 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class SWEA_1873_상호의_배틀필드 {
-    private static int H, W, D;
+    private static int H, W, D, r, c;
     private static char[][] map;
     private static final int[] dx = {-1, 0, 1, 0};
     private static final int[] dy = {0, 1, 0, -1};
     private static final char[] dc = {'^', '>', 'v', '<'}; // 상 우 하 좌
-
-    //        public static void main(String[] args) throws IOException{
+    private static StringBuilder sb;
+    // public static void main(String[] args) throws IOException{
     public static void test() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
+        sb = new StringBuilder();
 
         // TestCase
         int T = Integer.parseInt(br.readLine());
@@ -32,8 +33,9 @@ public class SWEA_1873_상호의_배틀필드 {
                 map[i] = br.readLine().toCharArray();
             }
 
-            setD();
+            setStart(); // 전차의 시작 위치와 방향
 
+            // play
             int n = Integer.parseInt(br.readLine());
             String orders = br.readLine();
             for (int i = 0; i < n; i++) {
@@ -41,36 +43,87 @@ public class SWEA_1873_상호의_배틀필드 {
 
                 switch (order) {
                     case 'U':
+                        turn(0);
+                        go();
                         break;
                     case 'D':
+                        turn(2);
+                        go();
                         break;
                     case 'L':
+                        turn(3);
+                        go();
                         break;
                     case 'R':
+                        turn(1);
+                        go();
                         break;
                     case 'S':
+                        shoot();
                         break;
+                }
+            }
+            sb.append('#').append(t).append(' ');
+            print(); // Output save
+        }
+        System.out.print(sb);
+    }
+    private static void turn(int d) {
+        map[r][c] = dc[d];
+        D = d;
+    }
+
+    private static void go() {
+        int nr = r + dx[D];
+        int nc = c + dy[D];
+
+        if (0 <= nr && nr < H && 0 <= nc && nc < W  && map[nr][nc] == '.') {
+            map[nr][nc] = map[r][c];
+            map[r][c] = '.';
+            r = nr;
+            c = nc;
+        }
+    }
+
+    private static void shoot() {
+        int nr = r + dx[D];
+        int nc = c + dy[D];
+
+        while (0 <= nr && nr < H && 0 <= nc && nc < W) {
+            if (map[nr][nc] == '*') {
+                map[nr][nc] = '.';
+                return;
+            }
+
+            if (map[nr][nc] == '#')
+                return;
+
+            nr += dx[D];
+            nc += dy[D];
+        }
+    }
+
+    private static void setStart() {
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                for (int k = 0; k < 4; k++) {
+                    if (map[i][j] == dc[k]) {
+                        D = k;
+                        r = i;
+                        c = j;
+                        return;
+                    }
                 }
             }
         }
     }
 
-    private static void setD() {
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < W; j++) {
-                if (map[i][j] == '<' || map[i][j] == '<' || map[i][j] == '<' || map[i][j] == '<')
-            }
-        }
-    }
-
     private static void print() {
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < W; j++) {
-                sb.append(map[i][j]).append(' ');
+                sb.append(map[i][j]);
             }
             sb.append('\n');
         }
-        System.out.println(sb);
     }
 }
