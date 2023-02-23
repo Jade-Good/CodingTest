0856b0e7911c0f88bb3c67da550ef2b2525748fe
BreathIN423 <kangsm423@gmail.com>
2023-02-23 22:05:25 +0900

    CT_170

--- BOJ/Java/src/S2/Boj_1012_유기농_배추.java
+++ BOJ/Java/src/S2/Boj_1012_유기농_배추.java
@@ -20,28 +20,45 @@ public class Boj_1012_유기농_배추 {
             int k = Integer.parseInt(st.nextToken());
 
             boolean[][] arr = new boolean[n][m];
+
             for (int i = 0; i < k; i++) {
                 st = new StringTokenizer(br.readLine());
-                arr[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = true;
+                int c = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                arr[r][c] = true;
             }
 
             int cnt = 0;
             Queue<Pair> que = null;
 
+            int[] dx = {-1, 0, 1, 0};
+            int[] dy = {0, 1, 0, -1};
+
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
                     if (arr[i][j]){
+                        cnt++;
+
                         que = new ArrayDeque<>();
+                        que.add(new Pair(i,j));
+                        arr[i][j] = false;
 
                         while(!que.isEmpty()) {
                             Pair pair = que.poll();
 
-
+                            for (int l = 0; l < 4; l++) {
+                                int x = pair.r+dx[l];
+                                int y = pair.c+dy[l];
+                                if (0 <= x && x < n && 0 <= y && y < m && arr[x][y]) {
+                                    arr[x][y] = false;
+                                    que.add(new Pair(x,y));
+                                }
+                            }
                         }
                     }
                 }
             }
-
+            System.out.println(cnt);
         }
     }
     private static class Pair {
