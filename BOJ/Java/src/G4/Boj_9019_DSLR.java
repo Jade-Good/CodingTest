368a39bf08eea0579b8941d8a479ddb84ae46a07
Soomin_Kang <kangsm423@gmail.com>
2023-03-21 22:40:33 +0900

    CT_190

--- BOJ/Java/src/G4/Boj_9019_DSLR.java
+++ BOJ/Java/src/G4/Boj_9019_DSLR.java
@@ -4,26 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Boj_9019_DSLR {
-    private static class Calc {
+    private static class DSLR {
         String history;
-        Deque<Integer> dq;
+        int num;
 
-        public Calc(String history, Deque<Integer> dq) {
+        public DSLR(int num, String history) {
+            this.num = num;
             this.history = history;
-            this.dq = dq;
-        }
-
-        @Override
-        public String toString() {
-            return "Calc{" +
-                    "history='" + history + '\'' +
-                    ", dq=" + dq +
-                    '}';
         }
     }
 
@@ -33,6 +24,7 @@ public class Boj_9019_DSLR {
         // Input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = null;
 
         for (int t = 0; t < T; t++) {
@@ -42,17 +34,17 @@ public class Boj_9019_DSLR {
             boolean[] visited = new boolean[MAX];
 
             // BFS
-            Queue<Calc> que = new ArrayDeque<>();
-            que.offer(new Calc("", make_Dq(A)));
-//            System.out.println(que);
+            Queue<DSLR> que = new ArrayDeque<>();
+            que.offer(new DSLR(A, ""));
 
             while (!que.isEmpty()) {
-                Calc calc = que.poll();
-                int num = make_num(calc.dq);
+                DSLR dslr = que.poll();
+                int num = dslr.num;
+                String history = dslr.history;
 
                 // B발견하면 종료
                 if (num == B) {
-                    System.out.println(calc.history);
+                    sb.append(history).append('\n');
                     break;
                 }
 
@@ -62,52 +54,30 @@ public class Boj_9019_DSLR {
                     // D 연산
                     int temp = (num * 2) % MAX;
                     if (!visited[temp]) {
-                        que.offer(new Calc(calc.history + "D", make_Dq(temp)));
+                        que.offer(new DSLR(temp, history + 'D'));
                     }
 
                     // S 연산
-                    temp = num == 0 ? 9999 : num-1;
-                    if(!visited[temp]) {
-                        que.offer(new Calc(calc.history + "S", make_Dq(temp)));
+                    temp = num == 0 ? 9999 : num - 1;
+                    if (!visited[temp]) {
+                        que.offer(new DSLR(temp, history + 'S'));
                     }
 
                     // L 연산
-                    calc.dq.addLast(calc.dq.pollFirst());
-                    temp = make_num(calc.dq);
-                    if(!visited[temp]) {
-                        que.offer(new Calc(calc.history + "L", make_Dq(temp)));
+                    temp = (num * 10) % 10000 + num / 1000;
+                    if (!visited[temp]) {
+                        que.offer(new DSLR(temp, history + 'L'));
                     }
 
                     // R 연산
-                    calc.dq.addFirst(calc.dq.pollLast());
-                    calc.dq.addFirst(calc.dq.pollLast());
-                    temp = make_num(calc.dq);
-                    if(!visited[temp]) {
-                        que.offer(new Calc(calc.history + "R", make_Dq(temp)));
+                    temp = num / 10 + num % 10 * 1000;
+                    if (!visited[temp]) {
+                        que.offer(new DSLR(temp, history + 'R'));
                     }
                 }
             }
         }
-    }
-
-    private static int make_num(Deque<Integer> dq) { // dq -> int
-        Deque<Integer> copy = new ArrayDeque<>(dq);
-        int num = 0;
-        while (!copy.isEmpty()) {
-            num = num * 10 + copy.pollFirst();
-        }
-        return num;
-    }
-
-    private static Deque<Integer> make_Dq(int num) { // int -> dq(0채우기)
-        Deque<Integer> result = new ArrayDeque<>();
-        while (num > 0) {
-            result.addFirst(num % 10);
-            num /= 10;
-        }
-        while (result.size() < 4) {
-            result.addFirst(0);
-        }
-        return result;
+        // Output
+        System.out.println(sb);
     }
 }
