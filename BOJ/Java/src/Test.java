947124bea1636b92331dcaa89cbf3510b7f8e4c4
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2023-04-24 02:22:18 +0900

    CT_218

---
+++ BOJ/Java/src/Test.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+//        for (int i = 100; i < 1000000; i+=100) {
+//            for (int j = 10; j <= 40; j+=10) {
+//                int a = 10 + (int) (i * (1-0.01*j));
+//                int b = (int) (10 + i * (1-0.01*j));
+//                if (a != b) {
+//                    System.out.println(i + " " + j);
+//                    System.out.println(a + " " + b);
+//                }
+//            }
+//        }
+
+        System.out.println(11700 * 0.7);
+        System.out.println(11700 * 70 / 100);
+    }
+}
