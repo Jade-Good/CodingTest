7aef72c3a31bba17af3bf727dd2a0b4ca3eec408
BreathIN423 <kangsm423@gmail.com>
2023-05-04 17:50:24 +0900

    CT_224

--- BOJ/Java/src/S1/Boj_5525_IOIOI.java
+++ BOJ/Java/src/S1/Boj_5525_IOIOI.java
@@ -1,7 +1,6 @@
 package S1;
 
 import java.io.BufferedReader;
-import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -11,29 +10,18 @@ public class Boj_5525_IOIOI {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        char[] S = br.readLine().toCharArray();
+        String S = br.readLine();
 
-        char[] target = new char[N * 2 + 1];
-
-        for (int i = 0; i <= N * 2; i += 2) {
-            target[i] = 'I';
-        }
-        for (int i = 1; i < N * 2; i += 2) {
-            target[i] = 'O';
+        int idx = 0;
+        while(idx < S.length()-2) {
+            if(S.substring(idx, idx+3).equals("IOI")) {
+                int tmp = 1;
+                while(S.substring())
+            }
         }
 
         int answer = 0;
-        for (int i = 0; i <= M-target.length; i++) {
-            boolean flag = true;
-            for (int j = 0; j < target.length; j++) {
-                if (S[i+j] != target[j]) {
-                    flag = false;
-                    break;
-                }
-            }
 
-            if (flag) answer++;
-        }
         System.out.print(answer);
     }
 }
