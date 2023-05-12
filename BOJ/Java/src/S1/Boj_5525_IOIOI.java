3d04275e79a57ecce56b5e4e57367042aca9afbf
BreathIN423 <kangsm423@gmail.com>
2023-05-12 17:52:13 +0900

    CT_230

--- BOJ/Java/src/S1/Boj_5525_IOIOI.java
+++ BOJ/Java/src/S1/Boj_5525_IOIOI.java
@@ -10,19 +10,42 @@ public class Boj_5525_IOIOI {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        String S = br.readLine();
-
-        int idx = 0;
-        while(idx < S.length()-2) {
-            if(S.substring(idx, idx+3).equals("IOI")) {
-                int tmp = 1;
-                idx += 3;
-//                while(S.substring())
-            }
-        }
+        char[] chars = br.readLine().toCharArray();
 
         int answer = 0;
+        int temp = 0;
+        boolean start = false;
+        boolean flag = false;
 
+        for (int i = 0; i < M; i++) {
+            if (chars[i] == 'I') { // I 발견
+                if (!start) { // 맨 앞 I
+                    start = true;
+                } else if (!flag) { // 연속 유지 I
+                    temp++;
+                } else { // 연속 끊는 I
+                    answer += cal(temp, N);
+                    temp = 0;
+                    start = true;
+                }
+                flag = true;
+            } else { // O 발견
+                if (!flag) {
+                    start = false;
+                    answer += cal(temp, N);
+                    temp = 0;
+                }
+                flag = false;
+            }
+        }
+        answer += cal(temp, N);
         System.out.print(answer);
+    }
+
+    private static int cal(int temp, int N) {
+        if (temp >= N) {
+            return temp - N + 1;
+        }
+        return 0;
     }
 }
