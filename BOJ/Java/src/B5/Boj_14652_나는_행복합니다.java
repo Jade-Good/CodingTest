b06d73489da08b4fefaa06af26183801406cd0d0
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2023-06-16 04:43:18 +0900

    CT #3

---
+++ BOJ/Java/src/B5/Boj_14652_나는_행복합니다.java
@@ -0,0 +1,23 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj_14652_나는_행복합니다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        bw.write(c/b + " " + c%b);
+        bw.flush();
+    }
+}
