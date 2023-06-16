ffb92779a4bfcfcf0b88e404d9d1240b3a76f4c3
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2023-06-17 01:26:00 +0900

    CT #4

---
+++ BOJ/Java/src/B5/Boj_15727_조별과제를_하려는데_조장이_사라졌다.java
@@ -0,0 +1,20 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj_15727_조별과제를_하려는데_조장이_사라졌다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+
+        bw.write(Integer.toString(t/5+(t%5==0?0:1)));
+        bw.flush();
+    }
+}
