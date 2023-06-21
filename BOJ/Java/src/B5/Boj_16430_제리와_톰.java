1206bb8e729089fde37d6321750a670ac9cf2c89
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2023-06-21 09:41:18 +0900

    CT #9

---
+++ BOJ/Java/src/B5/Boj_16430_제리와_톰.java
@@ -0,0 +1,24 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj_16430_제리와_톰 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        sb.append(b - a).append(' ').append(b);
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
