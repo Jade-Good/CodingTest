0701e76702e5e0b4adc5b1d57bb441df738a00db
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2023-06-24 00:30:57 +0900

    CT #11

---
+++ BOJ/Java/src/B5/Boj_27433_팩토리얼2.java
@@ -0,0 +1,22 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Boj_27433_팩토리얼2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        long sum = 1;
+        for (int i = 1; i <= n; i++) {
+            sum *= i;
+        }
+        bw.write(Long.toString(sum));
+        bw.flush();
+    }
+}
