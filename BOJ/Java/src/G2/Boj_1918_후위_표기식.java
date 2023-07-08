19730a9da2378cfdbe61dde00705cf598365204a
강수민 <kangsm423@gmail.com>
2023-07-08 23:11:46 +0900

    CT #24

--- BOJ/Java/src/G2/Boj_1918_후위_표기식.java
+++ BOJ/Java/src/G2/Boj_1918_후위_표기식.java
@@ -1,29 +1,32 @@
 package G2;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Boj_1918_후위_표기식 {
     private static class MyStack {
-        int pointer;
-        int[] stack;
+        private int pointer;
+        private char[] stack;
 
-        public MyStack(int pointer) {
-            this.pointer = pointer;
-            stack = new int[100];
+        public MyStack() {
+            this.pointer = 0;
+            stack = new char[100];
         }
 
-        public int push(int n) {
-            stack[pointer++] = n;
-            return pointer;
+        public void push(char c) {
+            stack[pointer++] = c;
         }
 
-        public int pop() {
-            return stack[pointer--];
+        public char pop() {
+            return stack[--pointer];
         }
 
         public boolean isEmpty() {
             return pointer == 0;
+        }
+
+        public char peek() {
+            if (pointer == 0) return '\0';
+            else return stack[pointer - 1];
         }
     }
 
@@ -31,7 +34,43 @@ public class Boj_1918_후위_표기식 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine(), ""); // A : 65, Z : 90
-        
+        char[] input = br.readLine().toCharArray(); // A : 65, Z : 90
+
+        MyStack stack = new MyStack();
+
+        for (char c : input) {
+            if (c >= 65 && c <= 90) // 입력 문자가 알파벳일 경우
+                sb.append(c);
+            else { // 입력 문자가 연산자일 경우
+                if (c == '+' || c == '-') { // '+' or '-'
+                    if (stack.peek() == '*' || stack.peek() == '/') {
+                        while (!stack.isEmpty() && stack.peek() != '(') {
+                            sb.append(stack.pop());
+                        }
+                    } else if (stack.peek() == '+' || stack.peek() == '-')
+                        sb.append(stack.pop());
+                    stack.push(c);
+                } else if (c == '*' || c == '/') { // '*' or '/'
+                    if (stack.peek() == '*' || stack.peek() == '/') {
+                        sb.append(stack.pop());
+                    }
+                    stack.push(c);
+                } else if (c == '(') { // '('
+                    stack.push(c);
+                } else { // ')'
+                    while (!stack.isEmpty()) {
+                        char p = stack.pop();
+                        if (p == '(') {
+                            break;
+                        }
+                        sb.append(p);
+                    }
+                }
+            }
+        }
+        while (!stack.isEmpty())
+            sb.append(stack.pop());
+        bw.write(sb.toString());
+        bw.flush();
     }
 }
