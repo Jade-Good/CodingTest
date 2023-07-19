00e33abae5e6312241adff27c3517d43652c90aa
Jade_Good <kangsm423@gmail.com>
2023-07-19 23:38:09 +0900

    CT #33

--- BOJ/Java/src/S1/Boj_9465_스티커.java
+++ BOJ/Java/src/S1/Boj_9465_스티커.java
@@ -1,6 +1,10 @@
 package S1;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Boj_9465_스티커 {
@@ -15,8 +19,8 @@ public class Boj_9465_스티커 {
         for (int tc = 0; tc < TC; tc++) {
             n = Integer.parseInt(br.readLine());
 
-            int[][] stk = new int[2][n+1];
-            int[][] dp = new int[2][n+1];
+            int[][] stk = new int[2][n + 1];
+            int[][] dp = new int[2][n + 1];
 
             st1 = new StringTokenizer(br.readLine());
             st2 = new StringTokenizer(br.readLine());
@@ -24,12 +28,15 @@ public class Boj_9465_스티커 {
                 stk[0][i] = Integer.parseInt(st1.nextToken());
                 stk[1][i] = Integer.parseInt(st2.nextToken());
             }
-
-            for (int i = 1; i <= n; i++) {
-                dp[0][i] = Math.max(stk[1][i+1], stk[1][i+1]) + stk[1][i];
-                dp[1][i] = Math.max(stk[0][i+1], stk[0][i+1]) + stk[0][i];
+            dp[0][1] = stk[0][1];
+            dp[1][1] = stk[1][1];
+            for (int i = 2; i <= n; i++) {
+                dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + stk[0][i];
+                dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + stk[1][i];
             }
+            sb.append(Math.max(dp[0][n], dp[1][n])).append('\n');
         }
-
+        bw.write(sb.toString());
+        bw.flush();
     }
 }
