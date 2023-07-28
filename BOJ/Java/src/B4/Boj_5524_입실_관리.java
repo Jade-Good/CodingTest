3042f2c2bbe7319d900535072ad41c09a256dda6
Jade_Good <kangsm423@gmail.com>
2023-07-29 00:35:31 +0900

    CT #40

---
+++ BOJ/Java/src/B4/Boj_5524_입실_관리.java
@@ -0,0 +1,23 @@
+package B4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Boj_5524_입실_관리 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            sb.append(br.readLine().toLowerCase()).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
