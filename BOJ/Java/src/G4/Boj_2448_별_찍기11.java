69554bc463710b9008e5d59fafc2fb15a73bad64
Jade_Good <kangsm423@gmail.com>
2023-08-28 22:58:04 +0900

    CT day 1

--- BOJ/Java/src/G4/Boj_2448_별_찍기11.java
+++ BOJ/Java/src/G4/Boj_2448_별_찍기11.java
@@ -7,90 +7,63 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Boj_2448_별_찍기11 {
-	private static StringBuilder sb;
+    private static StringBuilder sb;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
 
-		int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 1; j < N - i; j++) {
-				sb.append(' ');
-			}
-			// for (int j = 0; j < i * 2 - 1; j++) {
-			// 	sb.append('*');
-			// }
-			recursion(i);
-			sb.append('\n');
-		}
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N - i; j++) {
+                sb.append(' ');
+            }
+            recursion(i);
+            for (int j = 0; j < N - i; j++) {
+                sb.append(' ');
+            }
+            sb.append('\n');
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        bw.write(sb.toString());
+        bw.flush();
+    }
 
-		System.out.println(sb.toString());
+    private static void recursion(int n) {
+        if (n == 1) {
+            sb.append('*');
+        } else if (n == 2) {
+            sb.append('*');
+            sb.append(' ');
+            sb.append('*');
+        } else if (n == 3) {
+            for (int i = 0; i < 5; i++) {
+                sb.append('*');
+            }
+        } else {    // 재귀
+            int before_max = find_before_max(n);
+            recursion(n - before_max);
+            for (int i = 0; i < (before_max * 2 - n + 1) * 2 - 1; i++) {
+                sb.append(' ');
+            }
+            recursion(n - before_max);
+        }
+    }
 
-	}
+    private static int find_before_max(int n) {
+        if (n <= 3) {
+            return 0;
+        }
 
-	private static void recursion(int n) {
-		if (n == 0) {
-			sb.append('*');
-		}
-		else if (n == 1) {
-			sb.append('*');
-			sb.append(' ');
-			sb.append('*');
-		}
-		else if (n == 2) {
-			for (int i = 0; i < 5; i++) {
-				sb.append('*');
-			}
-		}
-		else if (n < 6) {
-			recursion(n%3);
-			for (int i = 0; i < (n / 3 * 6 - n) * 2 - 1; i++) {
-				sb.append(' ');
-			}
-			recursion(n%3);
-		}
-		else {    // 재귀
-			recursion(n%(n/6*6));
-			for (int i = 0; i < (n / 3 * 6 - n) * 2 - 1; i++) {
-				sb.append(' ');
-			}
-			recursion(n%(n/6*6));
-		}
-		/*
-		0
-		1
-		2
-		------------------3
-		3  -> 0
-		4  -> 1
-		5  -> 2
-		------------------6
-		6  -> 0
-		7  -> 1
-		8  -> 2
-		9  -> 3  -> 0
-		10 -> 4  -> 1
-		11 -> 5  -> 2
-		------------------12
-		12 -> 0
-		13 -> 1
-		14 -> 2
-		15 -> 3  -> 0
-		16 -> 4  -> 1
-		17 -> 5  -> 2
-		18 -> 6  -> 3 -> 0
-		19 -> 7  -> 4 -> 1
-		20 -> 8  -> 5 -> 2
-		21 -> 9  -> 6 -> 3 -> 0
-		22 -> 10 -> 7 -> 4 -> 1
-		23 -> 11 -> 8 -> 5 -> 2
-		------------------24
+        int i = 0;
+        double num = 3 * Math.pow(2, i);
 
+        while (num < n) {
+            num = 3 * Math.pow(2, ++i);
+        }
 
-		3 // 6 // 12
-		 */
-	}
+        return (int) (3 * Math.pow(2, i - 1));
+    }
 }
