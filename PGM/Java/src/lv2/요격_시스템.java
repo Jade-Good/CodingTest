fff9e5f50bb853f8b241b43680da6e0eb1360d0e
Jade_Good <kangsm423@gmail.com>
2023-11-29 16:46:58 +0900

    DayCT_1

--- PGM/Java/src/lv2/요격_시스템.java
+++ PGM/Java/src/lv2/요격_시스템.java
@@ -3,6 +3,7 @@ package lv2;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class 요격_시스템 {
 	public static void main(String[] args) throws IOException {
@@ -20,7 +21,18 @@ public class 요격_시스템 {
 
 	private static class Solution {
 		public int solution(int[][] targets) {
+			Arrays.sort(targets, (o1, o2) -> o1[1]-o2[1]);
+
+			int beforeEnd = 0;
 			int answer = 0;
+
+			for (int i = 0; i < targets.length; i++) {
+				if (beforeEnd <= targets[i][0]) {
+					beforeEnd = targets[i][1];
+					answer++;
+				}
+			}
+
 			return answer;
 		}
 	}
