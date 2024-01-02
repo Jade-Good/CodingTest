bf08185c45970fd0f8c7d5f18a6c30aef279e2eb
Jade_Good <kangsm423@gmail.com>
2024-01-03 02:11:26 +0900

    CT_4

---
+++ BOJ/Java/src/B5/Boj_29751_삼각형.java
@@ -0,0 +1,23 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj_29751_삼각형 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        double result = w * h * 0.5;
+
+        bw.write(Double.toString(Math.round(result * 10) / 10.0));
+        bw.flush();
+    }
+}
