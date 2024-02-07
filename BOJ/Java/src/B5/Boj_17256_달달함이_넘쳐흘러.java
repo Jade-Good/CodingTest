f2e83034938d17da3181c65d76ae6bfd9b08d404
Jade_Good <kangsm423@gmail.com>
2024-02-08 00:32:32 +0900

    CT_38

---
+++ BOJ/Java/src/B5/Boj_17256_달달함이_넘쳐흘러.java
@@ -0,0 +1,35 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj_17256_달달함이_넘쳐흘러 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int ax = Integer.parseInt(st.nextToken());
+        int ay = Integer.parseInt(st.nextToken());
+        int az = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int cx = Integer.parseInt(st.nextToken());
+        int cy = Integer.parseInt(st.nextToken());
+        int cz = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(cx - az).append(' ').append(cy / ay).append(' ').append(cz - ax);
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
+
+//bx = cx - az
+//by = cy / ay
+//bz = cz - ax
