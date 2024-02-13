54d700b238c98b845946de3817c9a3a9b67fb9a1
Jade_Good <kangsm423@gmail.com>
2024-02-13 23:47:04 +0900

    CT_44

---
+++ BOJ/Java/src/B5/Boj_18096_Арифметическая_магия.java
@@ -0,0 +1,13 @@
+package B5;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Boj_18096_Арифметическая_магия {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write('1');
+        bw.flush();
+    }
+}
