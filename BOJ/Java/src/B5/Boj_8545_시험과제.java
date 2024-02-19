6adcad5de4d220886bc8630f7b19059390ee4641
Jade_Good <kangsm423@gmail.com>
2024-02-20 00:55:01 +0900

    CT_50

---
+++ BOJ/Java/src/B5/Boj_8545_시험과제.java
@@ -0,0 +1,17 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Boj_8545_시험과제 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder(br.readLine()).reverse();
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
