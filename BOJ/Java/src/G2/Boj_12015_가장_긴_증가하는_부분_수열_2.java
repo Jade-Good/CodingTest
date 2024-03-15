86d3ba170206a4fc3a9561f405979bf1a59dcdb3
강수민 <kangsm423@gmail.com>
2024-03-15 12:23:47 +0900

    CT_75

---
+++ BOJ/Java/src/G2/Boj_12015_가장_긴_증가하는_부분_수열_2.java
@@ -0,0 +1,13 @@
+package G2;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Boj_12015_가장_긴_증가하는_부분_수열_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+    }
+}
