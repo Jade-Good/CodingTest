5877efb19cf92e2324cf4ec86b9fbe3efc575393
Jade_Good <kangsm423@gmail.com>
2024-03-21 23:26:19 +0900

    CT_81

---
+++ BOJ/Java/src/B5/Boj_31654_Adding_Trouble.java
@@ -0,0 +1,28 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj_31654_Adding_Trouble {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        if (A + B == C) {
+            bw.write("correct!");
+        } else {
+            bw.write("wrong!");
+        }
+        bw.flush();
+    }
+}
