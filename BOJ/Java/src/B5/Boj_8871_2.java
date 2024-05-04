2024468940a6046b0d4074a9a2903b6bbe302a26
강수민 <kangsm423@gmail.com>
2024-05-04 10:42:50 +0900

    CT_124

---
+++ BOJ/Java/src/B5/Boj_8871_2.java
@@ -0,0 +1,21 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Boj_8871_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+
+        sb.append(n * 2 + 2).append(' ').append(n * 3 + 3);
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
