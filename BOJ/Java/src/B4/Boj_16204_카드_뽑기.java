ae1a3372387e2419f23fe2e8e46d319c0472491a
강수민 <kangsm423@gmail.com>
2024-05-09 04:26:24 +0900

    CT_128

---
+++ BOJ/Java/src/B4/Boj_16204_카드_뽑기.java
@@ -0,0 +1,23 @@
+package B4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj_16204_카드_뽑기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        bw.write(Integer.toString(Math.min(M, K) + Math.min(N - M, N - K)));
+        bw.flush();
+    }
+}
