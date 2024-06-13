0f5fe6af145d8c873c20e9fdda5f20dbdeb4fe6a
강수민 <kangsm423@gmail.com>
2024-06-13 09:16:15 +0900

    CT_164

---
+++ BOJ/Java/src/B3/Boj_23530_NotAB.java
@@ -0,0 +1,22 @@
+package B3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Boj_23530_NotAB {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            sb.append(1).append('\n');
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
