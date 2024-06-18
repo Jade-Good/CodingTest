e32c26025e04c0472df22691087230bb17e28ff6
강수민 <kangsm423@gmail.com>
2024-06-18 21:44:22 +0900

    CT_170

---
+++ BOJ/Java/src/B4/Boj_13597_Tri_du.java
@@ -0,0 +1,26 @@
+package B4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj_13597_Tri_du {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if (A == B) {
+            bw.write(Integer.toString(A));
+        } else {
+            bw.write(Integer.toString(Math.max(A, B)));
+        }
+        bw.flush();
+    }
+}
