91a9c6c6f817855110a105888979cb4412d7e522
강수민 <kangsm423@gmail.com>
2024-08-12 23:56:58 +0900

    CT_221

---
+++ BOJ/Java/src/S4/Boj_30458_팰린드롬_애너그램.java
@@ -0,0 +1,42 @@
+package S4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Boj_30458_팰린드롬_애너그램 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        char[] chars = br.readLine().toCharArray();
+        int[] arr = new int[26];
+
+        for (char c : chars) {
+            arr[c - 'a']++;
+        }
+
+        if (N % 2 == 1) {
+            arr[chars[N / 2] - 'a']--;
+        }
+
+        boolean flag = true;
+        for (int i : arr) {
+            if (i % 2 != 0) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag) {
+            bw.write("Yes");
+        } else {
+            bw.write("No");
+        }
+        bw.flush();
+    }
+}
