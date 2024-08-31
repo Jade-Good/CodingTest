9bb413860f2b55f676e3ea82555f1691073834bb
강수민 <kangsm423@gmail.com>
2024-08-31 23:58:58 +0900

    CT_238

---
+++ BOJ/Java/src/S4/Boj_2491_수열.java
@@ -0,0 +1,28 @@
+package S4;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj_2491_수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = 2;
+
+        int now = arr[0], next;
+
+
+    }
+}
