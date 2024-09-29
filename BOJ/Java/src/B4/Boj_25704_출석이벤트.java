f39759a1245747bc200f77954dbee1f4d9d82c37
강수민 <kangsm423@gmail.com>
2024-09-30 00:51:40 +0900

    CT_266

--- BOJ/Java/src/B4/Boj_25704_출석이벤트.java
+++ BOJ/Java/src/B4/Boj_25704_출석이벤트.java
@@ -13,22 +13,27 @@ public class Boj_25704_출석이벤트 {
 
         int N = Integer.parseInt(br.readLine());
         int P = Integer.parseInt(br.readLine());
-        int min = Integer.MAX_VALUE;
 
-        if (N >= 5) {
-            min = Math.min(min, P - 500);
+        // 적용 가능한 할인 쿠폰을 계산
+        int finalPrice = P; // 할인 적용 후 가격
+
+        if (N >= 20) { // 25% 할인 쿠폰
+            finalPrice = Math.min(finalPrice, (int) (P * 0.75));
         }
-        if (N >= 10) {
-            min = Math.min(min, (int) (P * 0.9));
+        if (N >= 15) { // 2,000원 할인 쿠폰
+            finalPrice = Math.min(finalPrice, P - 2000);
         }
-        if (N >= 15) {
-            min = Math.min(min, P - 2000);
+        if (N >= 10) { // 10% 할인 쿠폰
+            finalPrice = Math.min(finalPrice, (int) (P * 0.9));
         }
-        if (N >= 20) {
-            min = Math.min(min, (int) (P * 0.75));
+        if (N >= 5) { // 500원 할인 쿠폰
+            finalPrice = Math.min(finalPrice, P - 500);
         }
 
-        bw.write(Integer.toString(min));
+        // 지불 금액이 0보다 작은 경우 0으로 설정
+        finalPrice = Math.max(finalPrice, 0);
+
+        bw.write(Integer.toString(finalPrice));
         bw.flush();
     }
 }
