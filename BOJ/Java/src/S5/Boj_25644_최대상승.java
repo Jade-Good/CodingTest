b83243efa580360789af09c3be32c7d76959ee22
강수민 <kangsm423@gmail.com>
2024-11-08 11:36:18 +0900

    CT_303

--- BOJ/Java/src/S5/Boj_25644_최대상승.java
+++ BOJ/Java/src/S5/Boj_25644_최대상승.java
@@ -10,7 +10,6 @@ public class Boj_25644_최대상승 {
 
         int max = 0;
         int min = Integer.MAX_VALUE;
-
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
