1d9f3487ed12060271ad68dd5c3e376c7c97ff58
강수민 <kangsm423@gmail.com>
2026-01-05 22:20:25 +0900

    CT_2

---
+++ BOJ/Java/src/B5/Boj_34849_이중_반복문.java
@@ -0,0 +1,23 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Boj_34849_이중_반복문 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if (N <= 10000) {
+            bw.write("Accepted");
+        } else {
+            bw.write("Time limit exceeded");
+        }
+        bw.flush();
+    }
+}
