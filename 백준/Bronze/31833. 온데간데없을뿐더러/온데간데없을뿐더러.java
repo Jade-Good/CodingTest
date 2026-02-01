f01af8f6252549847e135c7e58e156cc0f917f35
강수민 <kangsm423@gmail.com>
2026-02-01 12:15:31 +0900

    [Bronze III] Title: 온데간데없을뿐더러, Time: 92 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/31833. 온데간데없을뿐더러/온데간데없을뿐더러.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        char[] A = br.readLine().replaceAll(" ", "").toCharArray();
+        char[] B = br.readLine().replaceAll(" ", "").toCharArray();
+        char[] result = A.length < B.length ? A : B;
+
+        if (A.length == B.length) {
+            for (int i = 0; i < A.length; i++) {
+                if (A[i] < B[i]) {
+                    result = A;
+                    break;
+                } else if (A[i] > B[i]) {
+                    result = B;
+                    break;
+                }
+            }
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+    }
+}
