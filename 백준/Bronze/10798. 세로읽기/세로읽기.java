77f77e5dfc09d0d816cf05287424fafbf7af5a91
강수민 <kangsm423@gmail.com>
2026-02-02 18:29:13 +0900

    [Bronze I] Title: 세로읽기, Time: 172 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[][] chars = new char[5][15];
+
+        for (int i = 0; i < 5; i++) {
+            char[] temp = sc.nextLine().toCharArray();
+
+            for (int j = 0; j < temp.length; j++) {
+                chars[i][j] = temp[j];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (Character.isLetterOrDigit(chars[j][i]))
+                    sb.append(chars[j][i]);
+            }
+        }
+
+        System.out.print(sb.toString().trim());
+    }
+}
