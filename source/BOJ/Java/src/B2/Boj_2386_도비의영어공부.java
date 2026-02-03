cb8b4c57b8f406ff21674abe4d8b5e99df9455a5
강수민 <kangsm423@gmail.com>
2026-02-03 20:26:17 +0900

    CT_31

---
+++ source/BOJ/Java/src/B2/Boj_2386_도비의영어공부.java
@@ -0,0 +1,23 @@
+package B2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_2386_도비의영어공부 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        char[] chars;
+        while ((chars = br.readLine().toCharArray()).length > 1) {
+            int sum = 0;
+            for (int i = 2; i < chars.length; i++) {
+                if (chars[0] == chars[i] || chars[0] - 32 == chars[i]) {
+                    sum++;
+                }
+            }
+            sb.append(chars[0]).append(' ').append(sum).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
