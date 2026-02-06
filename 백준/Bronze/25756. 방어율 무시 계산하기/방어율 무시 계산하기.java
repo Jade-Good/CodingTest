58f2d7cb05d858b53c2784d835dba793c9d271d2
강수민 <kangsm423@gmail.com>
2026-02-06 23:34:06 +0900

    [Bronze III] Title: 방어율 무시 계산하기, Time: 324 ms, Memory: 28024 KB -BaekjoonHub

---
+++ 백준/Bronze/25756. 방어율 무시 계산하기/방어율 무시 계산하기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double V = 0;
+
+        for (int i = 0; i < N; i++) {
+            double A = Double.parseDouble(st.nextToken()) / 100.0;
+            V = V + A - V * A;
+            sb.append(V * 100).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
