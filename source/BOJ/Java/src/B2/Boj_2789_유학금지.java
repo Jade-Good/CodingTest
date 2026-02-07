29e778d711365bcf163e5304ad4a0c79d5069e25
강수민 <kangsm423@gmail.com>
2026-02-07 13:48:51 +0900

    CT_35

---
+++ source/BOJ/Java/src/B2/Boj_2789_유학금지.java
@@ -0,0 +1,34 @@
+package B2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Boj_2789_유학금지 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        char[] chars = "CAMBRIDGE".toCharArray();
+
+        Set<Character> set = new HashSet<>();
+        for (char c : chars) {
+            set.add(c);
+        }
+
+        chars = br.readLine().toCharArray();
+        for (char c : chars) {
+            if (!set.contains(c)) {
+                sb.append(c);
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
