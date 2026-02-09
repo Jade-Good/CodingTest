85000d3d3175083eb872a0434a55aa7cd8a77b8f
강수민 <kangsm423@gmail.com>
2026-02-09 20:04:51 +0900

    [Bronze I] Title: 반올림, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/4539. 반올림/반올림.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            int num = 10;
+
+            while (x > num) {
+                x = (x + 5 * num / 10) / num * num;
+                num *= 10;
+            }
+            sb.append(x).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
