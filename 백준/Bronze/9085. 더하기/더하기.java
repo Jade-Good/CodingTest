8600f9a8d00afd26aa2b40561f38c49b2a02c186
강수민 <kangsm423@gmail.com>
2026-02-10 22:58:57 +0900

    [Bronze IV] Title: 더하기, Time: 88 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            for (int n = 0; n < N; n++) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+            sb.append(sum).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
