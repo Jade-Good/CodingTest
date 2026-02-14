37ba9c47c7f9b25d1fedda6352eb93e5bc2ab1f5
강수민 <kangsm423@gmail.com>
2026-02-14 18:25:20 +0900

    CT_42

---
+++ source/BOJ/Java/src/B3/Boj_34758_KUPC에어서오세요.java
@@ -0,0 +1,35 @@
+package B3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj_34758_KUPC에어서오세요 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int X = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+
+        for (int n = 0; n < N; n++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (X == x || Y == y) {
+                sb.append(0).append('\n');
+            } else {
+                sb.append(1).append('\n');
+            }
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
