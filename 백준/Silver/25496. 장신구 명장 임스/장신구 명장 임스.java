a2644bc5b8a6fb3562e8737e04713191e7fc22d8
강수민 <kangsm423@gmail.com>
2026-02-17 13:58:53 +0900

    [Silver V] Title: 장신구 명장 임스, Time: 96 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Silver/25496. 장신구 명장 임스/장신구 명장 임스.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int P = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            if (P >= 200) break;
+            P += A[i];
+            cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
