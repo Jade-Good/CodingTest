a21cc1005d0dea56190c0c372bbce43128daacdc
강수민 <kangsm423@gmail.com>
2026-02-17 14:01:04 +0900

    CT_45

---
+++ source/BOJ/Java/src/S5/Boj_25496_장신구명장임스.java
@@ -0,0 +1,35 @@
+package S5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj_25496_장신구명장임스 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int P = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            if (P >= 200) break;
+            P += A[i];
+            cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
