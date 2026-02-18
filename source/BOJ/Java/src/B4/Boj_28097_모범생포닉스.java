c1f8c80a449c813dd1663a712fa214bd0a88f2ea
강수민 <kangsm423@gmail.com>
2026-02-18 19:30:29 +0900

    CT_46

---
+++ source/BOJ/Java/src/B4/Boj_28097_모범생포닉스.java
@@ -0,0 +1,27 @@
+package B4;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Boj_28097_모범생포닉스 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = -8;
+
+        for (int i = 0; i < N; i++) {
+            sum += Integer.parseInt(st.nextToken()) + 8;
+        }
+
+        sb.append(sum / 24).append(' ').append(sum % 24);
+        bw.write(sb.toString());
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
