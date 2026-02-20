42e8e8bad63de48e69c3f6a547b39b3475a40a49
강수민 <kangsm423@gmail.com>
2026-02-20 20:07:33 +0900

    CT_48

---
+++ source/BOJ/Java/src/B3/Boj_23805_골뱅이찍기돌아간ㄹ.java
@@ -0,0 +1,47 @@
+package B3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Boj_23805_골뱅이찍기돌아간ㄹ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        String large = "@".repeat(3 * N);
+        String small = "@".repeat(N);
+        String space = " ".repeat(N);
+
+        for (int i = 0; i < N; i++) {
+            sb.append(large);
+            sb.append(space);
+            sb.append(small);
+            sb.append('\n');
+        }
+        for (int i = 0; i < 3 * N; i++) {
+            sb.append(small);
+            sb.append(space);
+            sb.append(small);
+            sb.append(space);
+            sb.append(small);
+            sb.append('\n');
+        }
+        for (int i = 0; i < N; i++) {
+            sb.append(small);
+            sb.append(space);
+            sb.append(large);
+            sb.append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
