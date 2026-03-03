cde61931ea78f108d246f640f1968da0e4c7ce0b
강수민 <kangsm423@gmail.com>
2026-03-04 01:21:10 +0900

    [Bronze II] Title: 내 스트릭을 돌려내!, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/30395. 내 스트릭을 돌려내！/내 스트릭을 돌려내！.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int max = 0;
+        int cnt = 0;
+        boolean flag = false;
+
+        while (st.hasMoreTokens()) {
+            int p = Integer.parseInt(st.nextToken());
+            if (p > 0) {
+                cnt++;
+                flag = false;
+            } else if (flag) {
+                max = Math.max(max, cnt);
+                cnt = 0;
+                flag = false;
+            } else {
+                flag = true;
+            }
+        }
+        max = Math.max(max, cnt);
+        System.out.print(max);
+    }
+}
