86936bc8c7f6d4806beb8dfd552d9a76fe92e974
강수민 <kangsm423@gmail.com>
2026-03-11 01:32:31 +0900

    CT_62

---
+++ source/BOJ/Java/src/B2/Boj_9226_도깨비말.java
@@ -0,0 +1,40 @@
+package B2;
+
+import java.io.*;
+
+public class Boj_9226_도깨비말 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String S;
+
+        while (!(S = br.readLine()).equals("#")) {
+            int idx = -1;
+            char[] chars = S.toCharArray();
+
+            for (int i = 0; i < chars.length; i++) {
+                if ("aeiou".indexOf(chars[i]) != -1) {
+                    idx = i;
+                    break;
+                }
+            }
+
+            if (idx > -1) {
+                for (int i = idx; i < chars.length; i++) {
+                    sb.append(chars[i]);
+                }
+                for (int i = 0; i < idx; i++) {
+                    sb.append(chars[i]);
+                }
+                sb.append("ay\n");
+            } else {
+                sb.append(S).append("ay\n");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
