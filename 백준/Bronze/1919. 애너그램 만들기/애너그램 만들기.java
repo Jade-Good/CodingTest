ea949ebd0327d0a269c2801e2ba5dac8bb141267
강수민 <kangsm423@gmail.com>
2026-03-15 03:00:46 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] a = br.readLine().toCharArray();
+        char[] b = br.readLine().toCharArray();
+
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+
+        for (char c : a) {
+            cnt1[c - 'a']++;
+        }
+
+        for (char c : b) {
+            cnt2[c - 'a']++;
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < 26; i++) {
+            sum += Math.abs(cnt1[i] - cnt2[i]);
+        }
+
+        System.out.print(sum);
+    }
+}
