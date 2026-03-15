b554672cde2ffd35b48bdba9bdcb9d18433f3171
강수민 <kangsm423@gmail.com>
2026-03-15 23:30:45 +0900

    [Bronze I] Title: 이진수 나눗셈, Time: 212 ms, Memory: 20092 KB -BaekjoonHub

---
+++ 백준/Bronze/22950. 이진수 나눗셈/이진수 나눗셈.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String M = br.readLine();
+        int K = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (M.charAt(i) == '0') {
+                cnt++;
+            } else {
+                break;
+            }
+        }
+
+        if (cnt >= K || cnt == N) {
+            System.out.print("YES");
+        } else {
+            System.out.print("NO");
+        }
+    }
+}
