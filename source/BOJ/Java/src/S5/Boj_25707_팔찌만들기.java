4c300c000192c107257e6d9aa35529b327e27ed2
강수민 <kangsm423@gmail.com>
2026-03-16 20:53:46 +0900

    CT_68

---
+++ source/BOJ/Java/src/S5/Boj_25707_팔찌만들기.java
@@ -0,0 +1,33 @@
+package S5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj_25707_팔찌만들기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        long sum = 0;
+
+        for (int i = 1; i < N; i++) {
+            sum += Math.abs(arr[i] - arr[i - 1]);
+        }
+        sum += Math.abs(arr[0] - arr[N - 1]);
+
+        System.out.print(sum);
+    }
+}
