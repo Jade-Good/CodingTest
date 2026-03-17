2d3b581045ac2ad2e00f918e897c278a259bdbff
강수민 <kangsm423@gmail.com>
2026-03-18 01:48:32 +0900

    CT_69

---
+++ source/BOJ/Java/src/B5/Boj_34721_역사를걸으면동국이보이고.java
@@ -0,0 +1,24 @@
+package B5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Boj_34721_역사를걸으면동국이보이고 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            sb.append("I love DGU\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
