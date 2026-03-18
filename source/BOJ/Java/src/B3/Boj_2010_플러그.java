4bab9c0f1cd3ab34d0a5c7df862c289c66cf1aa9
강수민 <kangsm423@gmail.com>
2026-03-18 23:20:40 +0900

    CT_70

---
+++ source/BOJ/Java/src/B3/Boj_2010_플러그.java
@@ -0,0 +1,20 @@
+package B3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_2010_플러그 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += Integer.parseInt(br.readLine()) - 1;
+        }
+
+        System.out.print(sum + 1);
+    }
+}
