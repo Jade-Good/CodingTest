ee91cb60e8d7009c1aa5213d27fb02a8de201c03
강수민 <kangsm423@gmail.com>
2026-03-20 17:36:38 +0900

    [Bronze II] Title: 팰린드롬, Time: 100 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/10174. 팰린드롬/팰린드롬.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine().toLowerCase();
+
+            boolean isPelin = true;
+
+            for (int j = 0; j < s.length() / 2; j++) {
+                if (s.charAt(j) != s.charAt(s.length() - j - 1)) {
+                    isPelin = false;
+                    break;
+                }
+            }
+
+            sb.append(isPelin ? "Yes\n" : "No\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
