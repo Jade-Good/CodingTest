f8280ae3388b03d6389866d3c69da4762a1e3ef7
강수민 <kangsm423@gmail.com>
2026-03-24 20:51:06 +0900

    [Silver V] Title: 또 수열 문제야, Time: 168 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/31229. 또 수열 문제야/또 수열 문제야.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        arr[0] = 1;
+        int num = 2;
+
+        for (int i = 1; i < N; i++, num++) {
+            boolean flag = false;
+            arr[i] = num;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] * arr[j] % (arr[i] + arr[j]) == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) {
+                i--;
+            } else {
+                arr[i] = num;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append(' ');
+        }
+
+        System.out.print(sb);
+    }
+}
