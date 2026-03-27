65ca27b2b392db1db26a7e9d47a1f159b5b1fae6
강수민 <kangsm423@gmail.com>
2026-03-27 22:00:51 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 136 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int k = 0; k < 2; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    sb.append("@@@@@");
+                }
+                sb.append('\n');
+            }
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    sb.append('@');
+                }
+                sb.append('\n');
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append("@@@@@");
+            }
+            sb.append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
