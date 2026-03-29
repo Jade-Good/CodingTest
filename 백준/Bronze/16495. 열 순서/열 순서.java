1999549ffea9e4acb7ff53ec9eef6e5c17cab673
강수민 <kangsm423@gmail.com>
2026-03-29 23:34:03 +0900

    [Bronze I] Title: 열 순서, Time: 168 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/16495. 열 순서/열 순서.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+
+        long result = 0;
+
+        for (int i = 0; i < S.length(); i++) {
+            result += (S.charAt(S.length() - i - 1) - 'A' + 1) * (long) Math.pow(26, i);
+        }
+
+        System.out.print(result);
+    }
+}
