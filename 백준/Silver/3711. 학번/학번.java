02612c17a3b1af02b7897f615647fb572bbe3515
강수민 <kangsm423@gmail.com>
2026-03-31 00:47:56 +0900

    [Silver V] Title: 학번, Time: 184 ms, Memory: 77452 KB -BaekjoonHub

---
+++ 백준/Silver/3711. 학번/학번.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int G = Integer.parseInt(br.readLine());
+            int[] arr = new int[G];
+
+            for (int i = 0; i < G; i++) {
+                arr[i] = Integer.parseInt(br.readLine());
+            }
+
+            int m = 1;
+            int[] visited = new int[1000001]; // 최대 학번 기준
+
+            while (true) {
+                boolean valid = true;
+
+                for (int i = 0; i < G; i++) {
+                    int r = arr[i] % m;
+
+                    if (visited[r] == m) { // 이미 방문
+                        valid = false;
+                        break;
+                    }
+
+                    visited[r] = m;
+                }
+
+                if (valid) {
+                    System.out.println(m);
+                    break;
+                }
+
+                m++;
+            }
+        }
+    }
+}
