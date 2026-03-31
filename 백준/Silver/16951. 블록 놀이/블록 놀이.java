a4eb070144479c8aed88ce86585c96972731725a
강수민 <kangsm423@gmail.com>
2026-04-01 00:49:27 +0900

    [Silver IV] Title: 블록 놀이, Time: 132 ms, Memory: 15048 KB -BaekjoonHub

---
+++ 백준/Silver/16951. 블록 놀이/블록 놀이.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int minTime = N;
+
+        for (int h = 1; h <= 1000; h++) {
+            int currentTime = 0;
+            boolean isValid = true;
+
+            for (int i = 0; i < N; i++) {
+                int targetHeight = h + (i * K);
+
+                if (targetHeight < 1) {
+                    isValid = false;
+                    break;
+                }
+
+                if (A[i] != targetHeight) {
+                    currentTime++;
+                }
+            }
+
+            if (isValid) {
+                minTime = Math.min(minTime, currentTime);
+            }
+        }
+
+        System.out.println(minTime);
+    }
+}
