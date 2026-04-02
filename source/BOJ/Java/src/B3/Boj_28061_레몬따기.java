a057c87912bd1b148dc80e2a6daac08c4f537c3b
강수민 <kangsm423@gmail.com>
2026-04-03 00:01:23 +0900

    CT_84

---
+++ source/BOJ/Java/src/B3/Boj_28061_레몬따기.java
@@ -0,0 +1,23 @@
+package B3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_28061_레몬따기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            max = Math.max(max, Integer.parseInt(st.nextToken()) - (N - i));
+        }
+
+        System.out.print(max);
+    }
+}
