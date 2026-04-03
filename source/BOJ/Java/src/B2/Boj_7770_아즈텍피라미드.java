399e629ef615e1da9716185e2fb3c3ab75eeb9f5
강수민 <kangsm423@gmail.com>
2026-04-04 02:31:36 +0900

    CT_85

---
+++ source/BOJ/Java/src/B2/Boj_7770_아즈텍피라미드.java
@@ -0,0 +1,25 @@
+package B2;
+
+import java.util.Scanner;
+
+public class Boj_7770_아즈텍피라미드 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        long sum = 0;
+        int h = 0;
+
+        for (int i = 1; ; i++) {
+            long currentLayerBlocks = (long) i * i + (long) (i - 1) * (i - 1);
+            sum += currentLayerBlocks;
+
+            if (sum > n) {
+                break;
+            }
+            h++;
+        }
+
+        System.out.print(h);
+    }
+}
