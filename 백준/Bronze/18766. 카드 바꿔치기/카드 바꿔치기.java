4ca4972b28f1d67ab9a78f937622d0c50b924b31
강수민 <kangsm423@gmail.com>
2026-04-06 00:22:34 +0900

    [Bronze I] Title: 카드 바꿔치기, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/18766. 카드 바꿔치기/카드 바꿔치기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+
+            int n = Integer.parseInt(br.readLine());
+
+            String[] s1 = br.readLine().split(" ");
+            String[] s2 = br.readLine().split(" ");
+
+            Arrays.sort(s1);
+            Arrays.sort(s2);
+            
+            if (Arrays.equals(s1, s2)) {
+                sb.append("NOT CHEATER\n");
+            } else {
+                sb.append("CHEATER\n");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
