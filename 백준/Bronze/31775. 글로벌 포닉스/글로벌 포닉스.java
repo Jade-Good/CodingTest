26f92086dfe6b8b86e289b18d6e03915008ecc38
강수민 <kangsm423@gmail.com>
2026-04-09 00:42:31 +0900

    [Bronze IV] Title: 글로벌 포닉스, Time: 112 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/31775. 글로벌 포닉스/글로벌 포닉스.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char a = br.readLine().charAt(0);
+        char b = br.readLine().charAt(0);
+        char c = br.readLine().charAt(0);
+
+        if ((a == 'l' || b == 'l' || c == 'l') && (a == 'k' || b == 'k' || c == 'k') && (a == 'p' || b == 'p'
+                || c == 'p')) {
+            System.out.print("GLOBAL");
+        } else {
+            System.out.print("PONIX");
+        }
+    }
+}
