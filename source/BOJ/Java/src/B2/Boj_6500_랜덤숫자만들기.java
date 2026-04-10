395851fdc60ec28cbaad9a82963c42969eb4562e
강수민 <kangsm423@gmail.com>
2026-04-11 01:20:05 +0900

    CT_92

---
+++ source/BOJ/Java/src/B2/Boj_6500_랜덤숫자만들기.java
@@ -0,0 +1,40 @@
+package B2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Boj_6500_랜덤숫자만들기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String a = br.readLine();
+
+        while (!a.equals("0")) {
+
+            Set<String> set = new HashSet<>();
+            set.add(a);
+
+            while (true) {
+                int next = Integer.parseInt(a);
+                a = String.format("%08d", next * next).substring(2, 6);
+                if (set.contains(a)) {
+                    break;
+                }
+                set.add(a);
+            }
+
+            sb.append(set.size()).append('\n');
+            a = br.readLine();
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
